5248f80ad32580e264d478ac8dc86d875a872e3a
Josh R <jriver35@uncc.edu>
2020-09-02 13:15:27 -0400

    added closest value in BST solution

---
+++ JavaScriptAlgos/ClosestValueBinarySearchTree.js
@@ -0,0 +1,19 @@
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+  return findClosestValueInBstHelper(tree, target, tree.value);
+
+  function findClosestValueInBstHelper(tree, target, closest) {
+    if (tree === null) return closest;
+
+    if (Math.abs(target - closest) > Math.abs(tree.value - target))
+      closest = tree.value;
+
+    if (target < tree.value) {
+      return findClosestValueInBstHelper(tree.left, target, closest);
+    } else if (target > tree.value) {
+      return findClosestValueInBstHelper(tree.right, target, closest);
+    } else {
+      return closest;
+    }
+  }
+}
